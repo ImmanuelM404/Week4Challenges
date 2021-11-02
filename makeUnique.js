60e84fb8afe3e36afbc20fa44c77d883ba185c12
ImmanuelM404 <91708047+ImmanuelM404@users.noreply.github.com>
2021-11-02 08:07:09 -0700

    initialCommit

---
+++ makeUnique.js
@@ -0,0 +1,31 @@
+//Write your solution below:   
+//Challenge 'Make Unique String' 11/2 Christian 
+                                    // Nested Loop 
+// const removeDups = (string) => {
+//     let strArray = string.split('')
+//     for (let i = 0; i < strArray.length; i++){
+//         for(let j = i + 1; j < strArray.length; j++){
+//             strArray.filter(() =>{
+//             if(strArray[i] !== strArray[j]){
+//                        j++
+//             } else {
+//                 strArray.splice(j, 1)
+//             }
+//         })
+//     }
+// }
+// let newString = strArray.join('')
+// console.log(newString)
+// }
+
+// removeDups('helloworld')
+
+
+
+                            // Set method *** practice later 
+// function makeUnique(str){
+//     return str.prototype.concat(...new Set(str))
+// }
+
+// console.log(makeUnique('helloworld'))
+// //console.log((makeUnique('abcabc'))
